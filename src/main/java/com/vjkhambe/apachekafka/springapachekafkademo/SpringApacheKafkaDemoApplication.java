0aaaad33462f333b9b0db911cb3018201faffcd1
vjkhambe <vj.khambe@gmail.com>
2021-02-16 19:07:55 +0530

    initial commit

---
+++ src/main/java/com/vjkhambe/apachekafka/springapachekafkademo/SpringApacheKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vjkhambe.apachekafka.springapachekafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApacheKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApacheKafkaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vjkhambe/apachekafka/springapachekafkademo/SpringApacheKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vjkhambe.apachekafka.springapachekafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApacheKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
